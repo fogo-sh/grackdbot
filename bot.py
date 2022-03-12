281a5a03d9bc85a80f91768f5caeebfb08363d3a
Riley Flynn <riley@rileyflynn.me>
2022-03-12 20:18:29 -0330

    Basic project & user embeds

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import json
+from typing import Any, Dict
 
+import dateutil.parser
 import discord
 from discord import app_commands
 from gql import Client, gql
@@ -8,7 +10,7 @@ from pydantic import BaseSettings
 
 transport = AIOHTTPTransport(url="https://grackdb.fogo.sh/query")
 
-GET_USER_FOR_DISCORD_ACCOUNT_QUERY = gql(
+GET_DATA_FOR_DISCORD_ACCOUNT_QUERY = gql(
     """
     query($discord_id: String!) {
       discordAccounts(first: 1, where: { discordID: $discord_id }) {
@@ -20,6 +22,13 @@ GET_USER_FOR_DISCORD_ACCOUNT_QUERY = gql(
             }
             bot {
               id
+              project {
+                id
+                name
+                description
+                startDate
+                endDate
+              }
             }
           }
         }
@@ -48,23 +57,61 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 
+def render_project_embed(data: Dict[str, Any]) -> discord.Embed:
+    embed = discord.Embed()
+    embed.title = data["name"]
+    embed.url = f"https://grackdb.fogo.sh/project/{data['id']}"
+    embed.description = data["description"]
+    embed.colour = discord.Colour.blue()
+    embed.add_field(
+        name="Start Date",
+        value=f"<t:{int(dateutil.parser.isoparse(data['startDate']).timestamp())}>",
+    )
+    embed.add_field(
+        name="End Date",
+        value=f"<t:{int(dateutil.parser.isoparse(data['endDate']).timestamp())}>"
+        if data["endDate"] is not None
+        else "Ongoing",
+    )
+    return embed
+
+
+def render_user_embed(data: Dict[str, Any]) -> discord.Embed:
+    embed = discord.Embed()
+    embed.title = data["username"]
+    embed.url = f"https://grackdb.fogo.sh/user/{data['username']}"
+    embed.colour = discord.Colour.gold()
+    return embed
+
+
 @tree.context_menu(guild=guild, name="Lookup in GrackDB")
 async def lookup(interaction: discord.Interaction, user: discord.User):
     async with Client(transport=transport, fetch_schema_from_transport=True) as session:
         resp = await session.execute(
-            GET_USER_FOR_DISCORD_ACCOUNT_QUERY,
+            GET_DATA_FOR_DISCORD_ACCOUNT_QUERY,
             variable_values={"discord_id": str(user.id)},
         )
 
         if len(resp["discordAccounts"]["edges"]) == 0:
-            await interaction.response.send_message(
-                "That user could not be found in GrackDB.", ephemeral=True
+            embed = discord.Embed(
+                description="That account could not be found in GrackDB"
             )
+            embed.colour = discord.Colour.red()
+            await interaction.response.send_message(embed=embed)
             return
 
-        # account = resp["discordAccounts"]["edges"][0]["node"]
-
-        await interaction.response.send_message(f"```json\n{json.dumps(resp)}\n```")
+        account = resp["discordAccounts"]["edges"][0]["node"]
+        if account["owner"] is not None:
+            embed = render_user_embed(account["owner"])
+        elif account["bot"] is not None and account["bot"]["project"] is not None:
+            # TODO: Maybe try and figure a way to work the repo into this?
+            embed = render_project_embed(account["bot"]["project"])
+        else:
+            embed = discord.Embed(
+                description="Hmm, that account seems to exist but does not have an associated owner user or project. Check the data in GrackDB and try again."
+            )
+            embed.colour = discord.Colour.red()
+        await interaction.response.send_message(embed=embed)
 
 
 @client.event
